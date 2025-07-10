4e66dcbf45a47af04aa1fbeca0d6d6e7fa31a457
Vincent Hilario <vphilario7@gmail.com>
2025-07-10 16:10:17 -0500

    Configure Edge runtimes for api files

--- src/app/api/strands/route.ts
+++ src/app/api/strands/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server'
 
+export const runtime = 'edge'
+
 export async function GET() {
   try {
     const API_URL = process.env.API_URL || 'https://nyt-games-api.onrender.com/'
